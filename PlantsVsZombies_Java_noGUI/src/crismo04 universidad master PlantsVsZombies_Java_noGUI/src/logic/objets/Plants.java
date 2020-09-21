59ca5e95633090dd5157aaccc0d866c7af682e0b
crismom <47787192+crismom@users.noreply.github.com>
2020-09-21 18:29:35 +0200

    Revert "masive upload for traduction and debug"
    This reverts commit 5392c1021d383ac5d5cc73eff18db48bb54ed38f.

--- PlantsVsZombies_Java_noGUI/src/crismo04 universidad master PlantsVsZombies_Java_noGUI/src/logic/objets/Plants.java
+++
@@ -1,9 +0,0 @@
-package logic.objets;
-
-public abstract class Plants extends Character {
-	protected int suns;
-	
-	public Plants() {	
-		super();
-	}
-}
--- PlantsVsZombies_Java_noGUI/src/crismo04 universidad master PlantsVsZombies_Java_noGUI/src/print/GamePrinter.java
+++
@@ -1,5 +0,0 @@
-package print;
-
-public interface GamePrinter {
-	String PrinterGame();
-}
